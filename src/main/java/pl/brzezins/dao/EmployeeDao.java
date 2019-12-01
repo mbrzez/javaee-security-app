856b6ccd60f23847dc0527f8471556097238c863
Maks <maks@brzezins.pl>
2019-12-01 14:12:33 +0100

    Implementing and improving EmployeeDao

--- src/main/java/pl/brzezins/dao/EmployeeDao.java
+++ src/main/java/pl/brzezins/dao/EmployeeDao.java
@@ -5,29 +5,38 @@ import pl.brzezins.entity.Employee;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
+import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Optional;
 
 @Stateless
-public class EmployeeDao {
+public class EmployeeDao implements Dao<Employee> {
     @PersistenceContext(unitName = "mysqlds")
     EntityManager entityManager;
 
-    public void createUser(String name, String surname) {
-        entityManager.persist(new Employee());
+    @Override
+    public Optional<Employee> get(long id) {
+        return Optional.ofNullable(entityManager.find(Employee.class, id));
     }
 
-    public void removeUser() {
-        //TODO
+    @Override
+    public List<Employee> getAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("SELECT e FROM Employee e", Employee.class);
+        return query.getResultList();
     }
 
-    public void updateUser() {
-        //TODO
+    @Override
+    public void save(Employee employee) {
+        entityManager.persist(employee);
     }
 
-    public List<Employee> getAllEmployees() {
-        //TODO
+    @Override
+    public void update(Employee employee) {
+        entityManager.merge(employee);
+    }
 
-        return new ArrayList<Employee>();
+    @Override
+    public void delete(Employee employee) {
+        entityManager.remove(employee);
     }
 }
